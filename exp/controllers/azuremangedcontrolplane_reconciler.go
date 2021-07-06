0b778620ed7d38c733e03f6a239f4636b8d1d6ca
LochanRn <rajanaga_ra@srmuniv.edu.in>
2021-07-06 23:09:00 +0530

    name of aks cluster should be from cluster object and not amcp object.

--- exp/controllers/azuremangedcontrolplane_reconciler.go
+++ exp/controllers/azuremangedcontrolplane_reconciler.go
@@ -73,7 +73,7 @@ func (r *azureManagedControlPlaneReconciler) Reconcile(ctx context.Context, scop
 	}
 
 	managedClusterSpec := &managedclusters.Spec{
-		Name:                  scope.ControlPlane.Name,
+		Name:                  scope.Cluster.Name,
 		ResourceGroupName:     scope.ControlPlane.Spec.ResourceGroupName,
 		NodeResourceGroupName: scope.ControlPlane.Spec.NodeResourceGroupName,
 		Location:              scope.ControlPlane.Spec.Location,
