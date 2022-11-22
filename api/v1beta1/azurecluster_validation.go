69a5f209570f1862c55b202dbca96b13c86b091c
Akhilesh Verma <akhlesh.vermaofc@gmail.com>
2022-11-22 22:51:57 +0530

    pvt cluster changes

--- api/v1beta1/azurecluster_validation.go
+++ api/v1beta1/azurecluster_validation.go
@@ -19,6 +19,7 @@ package v1beta1
 import (
 	"fmt"
 	"net"
+	"os"
 	"reflect"
 	"regexp"
 
@@ -339,7 +340,8 @@ func validateAPIServerLB(lb LoadBalancerSpec, old LoadBalancerSpec, cidrs []stri
 					fldPath.Child("frontendIPConfigs").Index(0).Child("privateIP")); err != nil {
 					allErrs = append(allErrs, err)
 				}
-				if len(old.FrontendIPs) != 0 && old.FrontendIPs[0].PrivateIPAddress != lb.FrontendIPs[0].PrivateIPAddress {
+				lbIPAllocationMethod := os.Getenv("AZURE_APISERVER_LB_IP_ALLOCATION")
+				if lbIPAllocationMethod == "Static" && len(old.FrontendIPs) != 0 && old.FrontendIPs[0].PrivateIPAddress != lb.FrontendIPs[0].PrivateIPAddress {
 					allErrs = append(allErrs, field.Forbidden(fldPath.Child("name"), "API Server load balancer private IP should not be modified after AzureCluster creation."))
 				}
 			}
--- azure/services/loadbalancers/spec.go
+++ azure/services/loadbalancers/spec.go
@@ -20,6 +20,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-08-01/network"
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/pkg/errors"
+	"os"
 	infrav1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 	"sigs.k8s.io/cluster-api-provider-azure/azure"
 	"sigs.k8s.io/cluster-api-provider-azure/azure/converters"
@@ -161,15 +162,26 @@ func (s *LBSpec) Parameters(existing interface{}) (parameters interface{}, err e
 func getFrontendIPConfigs(lbSpec LBSpec) ([]network.FrontendIPConfiguration, []network.SubResource) {
 	frontendIPConfigurations := make([]network.FrontendIPConfiguration, 0)
 	frontendIDs := make([]network.SubResource, 0)
+	lbIPAllocationMethod := os.Getenv("AZURE_APISERVER_LB_IP_ALLOCATION")
+	var privateIPAllocationMethod network.IPAllocationMethod
+	if lbIPAllocationMethod == "Dynamic" {
+		privateIPAllocationMethod = network.IPAllocationMethodDynamic
+	} else {
+		privateIPAllocationMethod = network.IPAllocationMethodStatic
+	}
 	for _, ipConfig := range lbSpec.FrontendIPConfigs {
 		var properties network.FrontendIPConfigurationPropertiesFormat
+		var privateIPAddress string
+		if lbIPAllocationMethod == "Static" {
+			privateIPAddress = ipConfig.PrivateIPAddress
+		}
 		if lbSpec.Type == infrav1.Internal {
 			properties = network.FrontendIPConfigurationPropertiesFormat{
-				PrivateIPAllocationMethod: network.IPAllocationMethodStatic,
+				PrivateIPAllocationMethod: privateIPAllocationMethod,
 				Subnet: &network.Subnet{
 					ID: to.StringPtr(azure.SubnetID(lbSpec.SubscriptionID, lbSpec.VNetResourceGroup, lbSpec.VNetName, lbSpec.SubnetName)),
 				},
-				PrivateIPAddress: to.StringPtr(ipConfig.PrivateIPAddress),
+				PrivateIPAddress: to.StringPtr(privateIPAddress),
 			}
 		} else {
 			properties = network.FrontendIPConfigurationPropertiesFormat{
@@ -225,7 +237,7 @@ func getLoadBalancingRules(lbSpec LBSpec, frontendIDs []network.SubResource) []n
 					FrontendPort:            to.Int32Ptr(lbSpec.APIServerPort),
 					BackendPort:             to.Int32Ptr(lbSpec.APIServerPort),
 					IdleTimeoutInMinutes:    lbSpec.IdleTimeoutInMinutes,
-					EnableFloatingIP:        to.BoolPtr(false),
+					EnableFloatingIP:        to.BoolPtr(true),
 					LoadDistribution:        network.LoadDistributionDefault,
 					FrontendIPConfiguration: &frontendIPConfig,
 					BackendAddressPool: &network.SubResource{
