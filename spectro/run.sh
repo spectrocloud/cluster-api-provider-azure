3114196c3de7a5d95e8cb2a8635989c2488585e7
Amit Sahastrabuddhe <amit.sahastra@gmail.com>
2025-05-22 14:43:56 +0530

    feat: add aso manifests

--- spectro/run.sh
+++ spectro/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm generated/*
+rm -rf generated/*
+mkdir -p generated
 
 kustomize build --load-restrictor LoadRestrictionsNone global > ./generated/core-global.yaml
 kustomize build --load-restrictor LoadRestrictionsNone base > ./generated/core-base.yaml
