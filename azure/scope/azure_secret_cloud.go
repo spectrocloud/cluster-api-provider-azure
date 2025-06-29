f63563b9ab66b2d99243738efa0786e298d02627
YinzheZhang <yinzhe.zhang@spectrocloud.com>
2025-06-29 15:53:55 -0700

    Minor fix for the path reading

--- azure/scope/azure_secret_cloud.go
+++ azure/scope/azure_secret_cloud.go
@@ -15,9 +15,12 @@ const (
 )
 
 func init() {
+
 	_, log, done := tele.StartSpanWithLogger(context.Background(), "scope.AzureScope.init")
+	log.Info("AzureSecretCloud initializing")
 	defer done()
 	path := os.Getenv(AzureEnvironentFolderEnvName)
+	log.Info("Path is", path)
 	if path == "" {
 		return
 	}
@@ -29,8 +32,10 @@ func init() {
 
 	for _, file := range files {
 		if !file.IsDir() && strings.EqualFold(filepath.Ext(file.Name()), ".json") {
-			if env, err := azure.EnvironmentFromFile(file.Name()); err == nil {
+			if env, err := azure.EnvironmentFromFile(filepath.Join(path, file.Name())); err == nil {
 				azure.SetEnvironment(env.Name, env)
+				log.Info("loaded Azure environment from file", "EnvName", env.Name)
+				log.Info("loaded Azure environment from file", "filename", file.Name())
 			} else {
 				log.Error(err, "failed to load Azure environment from file", "filename", file.Name())
 			}
