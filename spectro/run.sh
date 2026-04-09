afb697953ceaa056a4ed5bb15bc8db40e970231f
Piyush Kumar <k17piyush@gmail.com>
2026-04-09 21:09:39 +0530

    Fix Spectro Manifest Generation Script (#131) (#143)
    * Fix Spectro Manifest Generation Script
    
    * Adding support for AKS cilium CNI
    
    Co-authored-by: Vishwanath Taykhande <54012750+vishu2498@users.noreply.github.com>

--- spectro/run.sh
+++ spectro/run.sh
@@ -4,3 +4,11 @@ rm generated/*
 
 kustomize build --load-restrictor LoadRestrictionsNone global > ./generated/core-global.yaml
 kustomize build --load-restrictor LoadRestrictionsNone base > ./generated/core-base.yaml
+
+# Fix ASO webhook service names: remove 'capz-' prefix from azureserviceoperator-webhook-service references
+sed -i.bak 's/capz-azureserviceoperator-webhook-service/azureserviceoperator-webhook-service/g' ./generated/core-global.yaml
+rm ./generated/core-global.yaml.bak
+
+sed -i.bak 's/capz-azureserviceoperator-webhook-service/azureserviceoperator-webhook-service/g' ./generated/core-base.yaml
+rm ./generated/core-base.yaml.bak
+
